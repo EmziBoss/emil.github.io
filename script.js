6f9cca91f11a06d032e0410857724fb1cc42e51b
EmziBoss <74204995+EmziBoss@users.noreply.github.com>
2023-12-28 20:59:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("generateButton").addEventListener("click", generateIdea);
+document.addEventListener("DOMContentLoaded", updateWeekNumber);
 
 function generateIdea() {
     const meat = ["Kyllinglår", "Kyllingvinger", "Kyllingkjøttdeig", "Kyllingfilet", "Svinefilet", "Svinestek", "Svinekjøttdeig", "Svinekoteletter", "Biff", "Oksekjøttdeig", "Grytekjøtt", "Biffstrimler", "Lammekoteletter", "Lammestek", "Lammebiff", "Laks", "Sei"];
@@ -29,11 +30,26 @@ function generateIdea() {
     }
 }
 
+function updateWeekNumber() {
+    const currentWeekNumber = getCurrentWeekNumber();
+    document.getElementById("weekNumber").textContent = `Uke ${currentWeekNumber}`;
+}
+
+function getCurrentWeekNumber() {
+    const today = new Date();
+    const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+    const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
+    
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
+
 function updateDinnerIdea(idea) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDay = days[new Date().getDay()];
-    const dayElement = document.getElementById(currentDay);
-
-    // Only update the span with the dinner idea
-    dayElement.querySelector(".dinner-idea").textContent = idea;
+    const norwegianDays = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
+    const currentDayIndex = new Date().getDay();
+    const currentDayNorwegian = norwegianDays[currentDayIndex];
+    
+    // Update the span with the dinner idea for the current Norwegian day
+    const dayElement = document.getElementById(currentDayNorwegian).querySelector(".dinner-idea");
+    dayElement.textContent = idea;
 }
