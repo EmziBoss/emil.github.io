7c4ce2b592c5370475b9d1d10fda4d3febc25eb5
EmziBoss <74204995+EmziBoss@users.noreply.github.com>
2023-12-28 20:47:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ function generateIdea() {
     const meat = ["Kyllinglår", "Kyllingvinger", "Kyllingkjøttdeig", "Kyllingfilet", "Svinefilet", "Svinestek", "Svinekjøttdeig", "Svinekoteletter", "Biff", "Oksekjøttdeig", "Grytekjøtt", "Biffstrimler", "Lammekoteletter", "Lammestek", "Lammebiff", "Laks", "Sei"];
     const vegetables = ["Gulrøtter", "Brokkoli", "Blomkål"];
     const pasta = ["Spagetti", "Penne", "Fusilli", "Makaroni", "Tagliatelle", "Nudler", "Orzo", "Linguine", "Fettuccine", "Kokte poteter", "Stekte poteter", "Ovnsbakte poteter", "Potetmos", "Pommes frites", "Hasselbakte poteter", "Fløtegrat", "Ris"];
-    const special = ["Pizza", "Burger", "Spagetti og kjøttdeig", "Taco", "Nachos", "Fiskegrateng", "Pitabrød", "Fish and Chips"];
+    const special = ["Pizza", "Burger", "Spagetti og kjøttdeig", "Taco", "Nachos", "Pitabrød", "Fish and Chips"];
 
     if (Math.random() < 0.20) {
-        // 20% chance to pick from the special category
         const selectedSpecial = special[Math.floor(Math.random() * special.length)];
         const idea = `Ta noe lett idag. Kjør ${selectedSpecial}?`;
-        document.getElementById("dinnerIdea").textContent = idea;
+        updateDinnerIdea(idea);
     } else {
-        // Regular selection process
         const selectedMeat = meat[Math.floor(Math.random() * meat.length)];
         const selectedPasta = pasta[Math.floor(Math.random() * pasta.length)];
-
         let selectedVegetables = [];
         let numberOfVegetables = Math.floor(Math.random() * 3) + 1; // 1, 2, or 3
 
@@ -28,6 +25,12 @@ function generateIdea() {
 
         const vegetableText = "med " + selectedVegetables.join(" og ");
         const idea = `Hva med ${selectedMeat} ${vegetableText} og ${selectedPasta}?`;
-        document.getElementById("dinnerIdea").textContent = idea;
+        updateDinnerIdea(idea);
     }
+}
+
+function updateDinnerIdea(idea) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = days[new Date().getDay()];
+    document.getElementById(currentDay).textContent = currentDay + ": " + idea;
 }
